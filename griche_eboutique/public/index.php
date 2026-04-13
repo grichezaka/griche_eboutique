473932bfac789898c27b9af91d831c9ddca394c3
grichezakae <zaka.griche@gmail.com>
2026-04-13 21:41:55 +0200

    Fix APP_DEBUG warning on prod

--- griche_eboutique/public/index.php
+++ griche_eboutique/public/index.php
@@ -5,9 +5,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
+
+$kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
-
